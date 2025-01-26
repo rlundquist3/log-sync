53fda921157e85050bbc349573c4a706adbb3c51
Riley Lundquist <rlundquist3@gmail.com>
2025-01-25 20:37:37 -0800

    fix mapping for old logs

--- src/parseNotes.js
+++ src/parseNotes.js
@@ -9,12 +9,26 @@ export const parseNotes = (notes) =>
           .split(/(-|\n)/)
           .filter((s) => !!s.match(gradeRegEx));
 
+        // Hack for paper log dates
+        let year;
+        const { title } = note;
+        if (title.includes("Old Paper Logs")) {
+          const yearsFromPaperLogs = ["2020", "2021", "2022", "2023"];
+          let index = 0;
+          while (!year) {
+            if (title.indexOf(yearsFromPaperLogs[index]) !== -1) {
+              year = yearsFromPaperLogs[index];
+            }
+            index += 1;
+          }
+        }
+
         return sends.map((s) => {
           const grade = s.match(gradeRegEx)[0];
           return {
             name: s.replace(`, ${grade}`, "").trim(),
             grade,
-            sendDate: new Date(note.createdAt),
+            sendDate: year ?? new Date(note.createdAt),
           };
         });
       }
